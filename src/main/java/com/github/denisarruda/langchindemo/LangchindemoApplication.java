c54cff4568824922b98d69dbe143df17db38ed6e
denis-arruda <denis.arruda@gmail.com>
2024-04-30 21:24:42 -0300

    Spring starter project

---
+++ src/main/java/com/github/denisarruda/langchindemo/LangchindemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.denisarruda.langchindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangchindemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LangchindemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/denisarruda/langchindemo/LangchindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.denisarruda.langchindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangchindemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
